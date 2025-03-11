c2b675cb34f49dd0f41aeddce4f834755ad26146
Glen Sharp <glen.sharp@ibm.com>
2025-03-11 22:53:02 +0000

    feat: updated data refresh script

--- emission-api/emission_calculator_backend/scripts/data-refresh.sh
+++ emission-api/emission_calculator_backend/scripts/data-refresh.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
-echo Delete SQLite DB file if present
-rm db.sqlite3
-echo
-
+# Enable exit on error
 set -e
 
 python manage.py test emission_calculator_backend/tests
 echo -----------------
 echo Unit tests passed
 echo -----------------
+
+# Disable exit on error for if SQLite file is not present
+set +e
+
+echo Delete SQLite DB file if present
+rm db.sqlite3
+echo
+
+# Enable exit on error
+set -e
 
 python manage.py makemigrations
 python manage.py migrate
